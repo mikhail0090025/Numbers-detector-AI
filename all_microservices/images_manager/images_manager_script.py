9af73f1c5df86376e0b38c72816ad986bcc40d89
Mikhail Ivashchenko <52649381+mikhail0090025@users.noreply.github.com>
2025-05-13 13:23:18 +0200

    upd

--- all_microservices/images_manager/images_manager_script.py
+++ all_microservices/images_manager/images_manager_script.py
@@ -1,6 +1,10 @@
 from PIL import Image, UnidentifiedImageError
 import numpy as np
 import os
+from pathlib import Path
+import requests
+import tarfile
+import shutil
 
 np.random.seed(42)
 
@@ -13,7 +17,14 @@ all_folders = [
     os.path.join("archive", "numbers", "chars74k_png", "Fnt"),
     os.path.join("archive", "numbers", "chars74k_png", "Hnd"),
 ]
+
 def get_images():
+    if Path(os.path.join("archive")).is_dir():
+        print("Dataset is found")
+    else:
+        print("Dataset was not found. Downloading...")
+        download_dataset()
+
     global images, outputs, images_are_loaded
     for folder in all_folders:
         for i in range(10):
@@ -42,3 +53,42 @@ def get_images():
     outputs = outputs[indexes]
     images_are_loaded = True
     print(f"Images found: {len(images)}")
+
+def download_dataset():
+    url = "http://www.ee.surrey.ac.uk/CVSSP/demos/chars74k/EnglishImg.tgz"
+    tar_path = "EnglishImg.tgz"
+    extract_path = "archive/numbers/chars74k_png"
+
+    if not Path("archive").is_dir():
+        print("Dataset was not found. Downloading...")
+        # Скачиваем файл
+        response = requests.get(url, stream=True)
+        if response.status_code == 200:
+            with open(tar_path, 'wb') as f:
+                f.write(response.raw.read())
+            print("Download complete. Extracting...")
+
+            # Распаковываем
+            with tarfile.open(tar_path, 'r:gz') as tar:
+                def is_within_directory(directory, target):
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    return prefix == abs_directory
+
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise ValueError("Attempted Path Traversal in Tar File")
+                    tar.extractall(path, members, numeric_owner=numeric_owner)
+
+                safe_extract(tar, extract_path)
+
+            # Удаляем .tgz после распаковки
+            os.remove(tar_path)
+            print(f"Dataset extracted to {extract_path}")
+        else:
+            raise Exception(f"Failed to download dataset. Status code: {response.status_code}")
+    else:
+        print("Dataset is found")
